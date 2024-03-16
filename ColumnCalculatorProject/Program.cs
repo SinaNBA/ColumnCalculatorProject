62c2f1f6e036a0b44c206326da3bd3de51150782
Sina Nejadbakhtiari <sina.nbakhtiari@gmail.com>
2024-03-16 18:05:35 +0100

    Step 1

---
+++ ColumnCalculatorProject/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ColumnCalculatorProject;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter the file path and the desired operator, separated by a space. The format should be 'FileName Operator(+ - * / r (meaning round) and e (meaning exponent))'.");
+    }
+}
