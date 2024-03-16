d39839b4dc187d2b7d1e540abd04de3f26756c1f
Sina Nejadbakhtiari <sina.nbakhtiari@gmail.com>
2024-03-16 17:45:36 +0100

    Revert "Step 1"
    This reverts commit 862fe3adab8e398377e88a8f316f9e429de631d6.

--- ColumnCalculatorProject/Program.cs
+++ ColumnCalculatorProject/Program.cs
@@ -4,52 +4,37 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please enter the file path and the desired operator, separated by a space. The format should be 'FileName Operator(+ - * / r (meaning round) and e (meaning exponent))'.");
-        bool validInput = false;
-        List<int> tempArrayToKeepinputs;
-        List<List<int>>? numberPairsList = new List<List<int>>();
+        Console.WriteLine("Enter your file path:");
+        bool validInput=false;
+        string[]? tempArrayToKeepinputs;
+        List<int>? resultValues=new List<int>();
 
         while (!validInput)
         {
             //D:\Projects\Peter's homeworks\8\Input1.csv
 
-            string? filePathAndOperator;
-            filePathAndOperator = Console.ReadLine();
-            string filePath;
-            string operatorInput;
+            string? filePath = Console.ReadLine();
 
-            if (!string.IsNullOrEmpty(filePathAndOperator))
+            if (filePath == null || !File.Exists(filePath))
             {
-                operatorInput = filePathAndOperator.Substring(filePathAndOperator.Length-1,1);
-                filePath=filePathAndOperator.Substring(0,filePathAndOperator.Length-1);
-
-                if (!File.Exists(filePath))
-                {
-                    Console.WriteLine("The file path does not exist.");
-                }
-                else
-                {
-                    var reader = new StreamReader(filePath);
-                    string? line;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        Console.WriteLine(line);
-                        tempArrayToKeepinputs = line.Split(',').Select(int.Parse).ToList();
-                        numberPairsList.Add(new List<int>(tempArrayToKeepinputs));
-
-                    }
-
-                    validInput = true;
-                }
+                Console.WriteLine("The file path is empty or does not exist.");
             }
             else
             {
-                Console.WriteLine("The file path is empty!");
-            }
+                var reader = new StreamReader(filePath);
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    tempArrayToKeepinputs = line.Split(',');
+                    resultValues.Add(tempArrayToKeepinputs.Sum(int.Parse));                   
+                    
+                }
 
-            
+                validInput= true;
+            }
         }
-        foreach (var element in numberPairsList)
+        foreach(var element in resultValues)
         {
             Console.WriteLine(element);
         }
